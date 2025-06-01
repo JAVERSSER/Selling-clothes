7f4d4ea19ddbae15abc4f46d8cf3dd113e7c0a36
Your Name <you@example.com>
2025-06-01 14:38:57 +0700

    Init

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <ScrollToTop />
-      <Routes basename="/">
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
